546c99a3151c3bc52a1fcfa94a4f4be0c829e269
lcwat <lcwatson012@gmail.com>
2024-12-02 13:29:11 -0600

    new maps

--- scripts/map-visuals.R
+++ scripts/map-visuals.R
@@ -8,15 +8,19 @@
 
 library(tidyverse)
 library(haven) # read spss sav files
-library(usmap) # plotting us map
+library(readxl) # read xlsx
+#library(usmap) # plotting us map
 library(haven) # read sav
 library(tidycensus) # tidy us census data
 library(tigris) # county area for density
 library(sf) # plotting maps
+library(crsuggest) # suggest best cartographic set for dataset
 
 # load data ---------------------------------------------------------------
 
-life_history_data_long <- read_sav("data/Final Project Data_long.sav")
+# life_history_data_long <- read_sav("data/Final Project Data_long.sav")
+life_history_data_long <- read_csv("data/life-hist-data-long.csv")
+life_history_data_wide <- read_xlsx("data/Final Project Data.xlsx")
 
 # take a look at variables with colnames, str, and summary
 colnames(life_history_data_long)
@@ -55,10 +59,14 @@ pop_county <- pop_county |>
   )
 
 
-# -------------------------------------------------------------------------
+# merge w county sf file for plotting -----------------------------------------
 
 # get sf data for plotting
 area_county <- counties(cb = TRUE)
+
+# rearrange geo to keep aleutian islands from expanding plot too much
+# best for national mapping like I plan to do
+area_county <- shift_geometry(area_county)
 
 # get area
 area_county <- area_county |> 
@@ -77,12 +85,30 @@ area_county <- area_county |>
 
 head(area_county)
 
+# remove NA locations
+plot_me <- life_history_data_long |> 
+  filter(!is.na(location))
+
 # can merge w data for plotting with sf
-life_history_data_long <- life_history_data_long |> 
-  left_join(area_county, by = join_by("Location" == "location"))
+plot_counties <- area_county |> 
+  left_join(plot_me, by = join_by("location" == "location"))
+
+# keep only contig us
+plot_counties <- plot_counties |> 
+  filter(statefips <= 56)
+
+#
+
+# what was cleaned --------------------------------------------------------
 
 scan <- life_history_data_long |> 
-  filter(is.na(geo_id))
+  filter(is.na(geo_id)) |> 
+  filter(is.na(Location) & !is.na(SexRatio))
+
+scan <- scan[, 1:23]
+
+# save file to debug
+write_csv(scan, "data/missing-location-but-data-present.csv")
 
 # appears to be some locations that are not present but have data, while some
 # others appear to not have merged with the df either to different names or 
@@ -90,7 +116,7 @@ scan <- life_history_data_long |>
 myc <- counties(cb = F)
 
 myc |> 
-  filter(str_detect(NAMELSAD, "El Paso"))
+  filter(str_detect(NAMELSAD, "San Diego"))
 
 mys <- states()
 
@@ -102,74 +128,52 @@ myc |>
   filter(STATEFP == "48")
 # somewhere along the way, the counties became planning regions, 
 # fairfield county is a part of the Greater Bridgeport Planning Region now
-# "Fairfield County" == "Greater Bridgeport Planning Region"
+# "Fairfield County" == "Greater Bridgeport Planning Region, Connecticut"
 
 life_history_data_long |> 
-  filter(str_detect(Location, "Tom Green")) |> 
+  filter(str_detect(Location, "San Diego")) |> 
   select(Location)
 
 # also Saint should not be spelled out, St. instead
-# "Saint Clair County, Missouri" == "St. Clair County, Missouri" MO
-# "Saint Clair County, Illinois" == "St. Clair County, Illinois" IL
-# "Saint Charles County, Missouri" == "St. Charles County, Missouri" MO
-# "El Paso County, Kansas" == "El Paso County, 
-
-# misspellings
+# "Saint Clair County, Missouri" == "St. Clair County, Missouri"
+# "Saint Clair County, Illinois" == "St. Clair County, Illinois"
+# "Saint Charles County, Missouri" == "St. Charles County, Missouri"
+# "El Paso County, Kansas" == "El Paso County, Texas"
 # "Sedwick County, Kansas" == "Sedgwick County, Kansas"
 # "Rockwall, Texas" == "Rockwall County, Texas"
 # "Bethel Census Area" == "Bethel Census Area, Alaska"
 # "Southeast Fairbanks Census Area, Kansas" == "Southeast Fairbanks Census Area, Alaska"
 # "Tom Green County, Kansas" == "Tom Green County, Texas"
-# 
+# "San Diego County, Kansas" == "San Diego County, California"
 
-
-# pop density -------------------------------------------------------------
-
-pop_dens <- left_join(areas, pop_county)
-
-pop_dens <- pop_dens |> 
+# use case when to replace misspellings
+life_history_data_long <- life_history_data_long |> 
   mutate(
-    # pop per square mile
-    density = pop / land_area * 1000 / .3861
+    location = case_when(
+      Location == "Saint Clair County, Missouri" ~ "St. Clair County, Missouri",
+      Location == "Saint Clair County, Illinois" ~ "St. Clair County, Illinois",
+      Location == "Saint Charles County, Missouri" ~ "St. Charles County, Missouri",
+      Location == "El Paso County, Kansas" ~ "El Paso County, Texas",
+      Location == "Sedwick County, Kansas" ~ "Sedgwick County, Kansas",
+      Location == "Rockwall, Texas" ~ "Rockwall County, Texas",
+      Location == "Bethel Census Area" ~ "Bethel Census Area, Alaska",
+      Location == "Southeast Fairbanks Census Area, Kansas" ~ "Southeast Fairbanks Census Area, Alaska",
+      Location == "Tom Green County, Kansas" ~ "Tom Green County, Texas",
+      Location == "San Diego County, Kansas" ~ "San Diego County, California",
+      Location == "Fairfield County, Connecticut" ~ "Greater Bridgeport Planning Region, Connecticut",
+      .default = Location
+    )
   )
 
-pop_dens |> 
-  arrange(desc(density))
+# save fixes
+write_csv(life_history_data_long, "data/life-hist-data-long.csv")
 
-# find relevant vars through filter
-sexvars <- var_table |> 
-  filter(str_detect(concept, "SEX"))
 
-# total female = P012026
-pop_female <- get_decennial(
-  geography = "county", 
-  variables = "P012026", 
-  year = 2010
-)
+# wide data ---------------------------------------------------------------
 
-# sex ratio
-pop_female <- pop_female |> 
-  select(
-    geo_id = GEOID,
-    pop_female = value
-  )
-
-pop_county <- left_join(pop_county, pop_female)
-
-pop_county <- pop_county |> 
-  mutate(
-    sex_ratio = pop_female / pop
-  )
-
-summary(pop_county$sex_ratio) # looks good
-
-# want variables sex ratio, life expectancy, pop density, income by county
-
-# mapping locations -------------------------------------------------------
-
-# us map code
+# orig wide data file
 # need to put data into map appropriate format state abbrev and county
-life_history_data_long <- cbind(
+life_history_data_wide <- cbind(
   life_history_data_long, 
   colsplit(life_history_data_long$Location, ", ", names = c("county", "state"))
 )
@@ -232,37 +236,119 @@ locations_all <- locations_all |>
 locations_all <- locations_all |> 
   rename(fips = r1_county_fips)
 
-locations_to_include <- locations_all$r1_state_abb
 
-plot_usmap(regions = "counties", data = locations_all, include = locations_to_include, values = "RiskTaking", color = "black") + 
-  scale_fill_continuous(
-    low = "green4", high = "green", 
-    name = "Risk Taking"
-  ) + 
-  theme(legend.position = "right")
+# pop density -------------------------------------------------------------
 
-plot_usmap(data = countypov, regions = "counties", values = "pct_pov_2021", color = "blue") +
-  scale_fill_continuous(
-    low = "blue4", high = "orange", 
-    name = "poverty percentage"
+pop_dens <- left_join(areas, pop_county)
+
+pop_dens <- pop_dens |> 
+  mutate(
+    # pop per square mile
+    density = pop / land_area * 1000 / .3861
   )
 
+pop_dens |> 
+  arrange(desc(density))
+
+# find relevant vars through filter
+sexvars <- var_table |> 
+  filter(str_detect(concept, "SEX"))
+
+# total female = P012026
+pop_female <- get_decennial(
+  geography = "county", 
+  variables = "P012026", 
+  year = 2010
+)
+
+# sex ratio
+pop_female <- pop_female |> 
+  select(
+    geo_id = GEOID,
+    pop_female = value
+  )
+
+pop_county <- left_join(pop_county, pop_female)
+
+pop_county <- pop_county |> 
+  mutate(
+    sex_ratio = pop_female / pop
+  )
+
+summary(pop_county$sex_ratio) # looks good
+
+# want variables sex ratio, life expectancy, pop density, income by county
+
+# mapping locations -------------------------------------------------------
+
+# us map code
+# locations_to_include <- locations_all$r1_state_abb
+# 
+# plot_usmap(regions = "counties", data = locations_all, include = locations_to_include, values = "RiskTaking", color = "black") + 
+#   scale_fill_continuous(
+#     low = "green4", high = "green", 
+#     name = "Risk Taking"
+#   ) + 
+#   theme(legend.position = "right")
+# 
+# plot_usmap(data = countypov, regions = "counties", values = "pct_pov_2021", color = "blue") +
+#   scale_fill_continuous(
+#     low = "blue4", high = "orange", 
+#     name = "poverty percentage"
+#   )
+
+# plot sf
 states <- states()
 
 states <- states |> 
   filter(STATEFP <= 56)
 
+# rearrange geo to keep aleutian islands from expanding plot too much
+states <- shift_geometry(states)
+
+# suggest crs
+crsuggest::suggest_crs(plot_counties)
+
+# same as already using, NAD83
+
+# count number of obs from counties, some overrepresented in convenience sample data
+sum_locations <- plot_counties |> 
+  group_by(location) |> 
+  summarise(
+    total = sum(!is.na(DDk))
+  )
+
+# turn zero to na
+sum_locations <- sum_locations |> 
+  mutate(
+    new_total = if_else(total == 0, NA, total)
+  )
+
 # plot with sf
-states |> 
+sum_locations |> 
   ggplot() +
   
-  geom_sf() +
-  
   geom_sf(
-    data = life_history_data_long, 
-    aes(geometry = geometry, fill = PopulationDensity)
+    aes(fill = new_total), 
+    color = NA
   ) + 
   
-  theme_void()
+  geom_sf(
+    data = states, 
+    fill = NA, color = "grey95"
+  ) +
   
+  # fill the counties with data
+  scale_fill_viridis_c(
+    "Number Reported",
+    option = "magma", na.value = "black",
+    begin = .5, end = 1
+  ) +
+  
+  theme_void()
+
+ggsave(
+  "plots/subj-by-county.png", device = "png",
+  width = 12, height = 10, units = "in"
+)
   
